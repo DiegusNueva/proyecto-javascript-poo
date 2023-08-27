90742cd14013638e25301854ea3dcc5425737d6d
Diego Alonso Molina <diegusnueva@gmail.com>
2023-08-27 19:59:10 +0200

    FINALIZADO:  Certificación de Beto personal

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,32 @@ import Alumno from "./classes/Alumno.js";
 
 const elem = document.getElementById("cursos");
 
+function resultadosCheckbox(target) {
+  // Obtener valores de checkboxes
+  const htmlCheckbox = target.html.checked;
+  const cssCheckbox = target.css.checked;
+  const javascriptCheckbox = target.javascript.checked;
+  const resultadoCheckbox = [];
+
+  if (htmlCheckbox) {
+    resultadoCheckbox.push("HTML");
+  }
+
+  if (cssCheckbox) {
+    resultadoCheckbox.push("CSS");
+  }
+
+  if (javascriptCheckbox) {
+    resultadoCheckbox.push("JavaScript");
+  }
+
+  resultadoCheckbox.forEach((element) => {
+    console.log(element);
+  });
+
+  return resultadoCheckbox;
+}
+
 function mostrarCurso(curso) {
   const hijo = document.createElement("div");
   hijo.classList.add("card");
@@ -21,11 +47,15 @@ function mostrarCurso(curso) {
   elem.appendChild(hijo);
 }
 
-const formulario = document.getElementById("formCursos");
-formulario.addEventListener("submit", (e) => {
+//DATOS obtenidos del formulario relativos al CURSO
+const formularioCurso = document.getElementById("formCursos");
+formularioCurso.addEventListener("submit", (e) => {
   e.preventDefault();
-  //console.log(e.target);
+  console.log(e.target);
   const target = e.target;
+  console.log(target.nombreCurso.value);
+  console.log(target.posterCurso.value);
+  console.log(target.clasesCurso.value);
   const curso = new Curso(
     target.nombreCurso.value,
     target.posterCurso.value,
@@ -34,19 +64,55 @@ formulario.addEventListener("submit", (e) => {
   mostrarCurso(curso);
 });
 
-const profe = new Profesor(
-  "Diego",
-  "Alonso",
-  "diego@mail.com",
-  true,
-  ["React", "Drupal"],
-  10
-);
+//DATOS obtenidos del formulario relativos al PROFESOR
+const formularioProfesores = document.getElementById("formProfesores");
+formularioProfesores.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(e.target);
+  const target = e.target;
+  console.log(target.nombreProfesor.value);
+  console.log(target.apellidosProfesor.value);
+  console.log(target.emailProfesor.value);
+  console.log(target.gender.value);
 
-const alumno1 = new Alumno("Juanito", "Pérez", "juan@mail.com", false, [
-  "HTML",
-  "CSS",
-]);
+  const valoresCheckboxProfesor = resultadosCheckbox(target);
+
+  console.log(target.calificacionProfesor.value);
+
+  const profe = new Profesor(
+    target.nombreProfesor.value,
+    target.apellidosProfesor.value,
+    target.emailProfesor.value,
+    target.gender.value,
+    valoresCheckboxProfesor,
+    target.calificacionProfesor.value
+  );
+});
+
+//DATOS obtenidos del formulario relativos al ALUMNO
+const formularioAlumnos = document.getElementById("formAlumnos");
+formularioAlumnos.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(e.target);
+  const target = e.target;
+  console.log(target.nombreAlumno.value);
+  console.log(target.apellidosAlumno.value);
+  console.log(target.emailAlumno.value);
+  console.log(target.gender.value);
+
+  const valoresCheckboxAlumno = resultadosCheckbox(target);
+
+  const alumno1 = new Alumno(
+    target.nombreAlumno.value,
+    target.apellidosAlumno.value,
+    target.emailAlumno.value,
+    target.gender.value,
+    valoresCheckboxAlumno
+  );
+
+  html.setInscritos([...html.getInscritos(), alumno1]);
+  console.log(html);
+});
 
 const alumno2 = new Alumno("María", "Alonso", "maria@mail.com", true, [
   "HTML",
@@ -56,8 +122,5 @@ const alumno2 = new Alumno("María", "Alonso", "maria@mail.com", true, [
 
 const html = new Curso("HTML desde cero", "mi-poster.png", 7);
 
-html.setInscritos([...html.getInscritos(),alumno1]);
-console.log(html);
-
-html.setInscritos([...html.getInscritos(),alumno2]);
+html.setInscritos([...html.getInscritos(), alumno2]);
 console.log(html);
